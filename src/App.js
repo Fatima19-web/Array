a0e68405005dba2d8ff22b1886abcfa1b80756d2
fatima <faizamanzoor947@gmail.com>
2025-12-05 22:36:56 +0500

    Replace existing code with new code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-// App.js
 import React from "react";
 
-export default 
-function App() {
+function FruitList() {
+  const fruits = ["Mango", "Banana", "Apple", "Orange", "Grapes"];
+
   return (
-    <div className="app-wrapper">
-      <div className="card">
-       <img src="/myPic.jpg" alt="My Pic"/>
-      <h2 className="name">Fatima Khan</h2>
-
-        <p><i>A girl who loves to enjoy her life, chasing her dreams with a fearless heart.
-          She believes in exploring new places, meeting new people, and discovering the
-          beauty hidden in every corner of the world. Travelling gives her freedom,
-          learning, and happiness. She lives her life with passion, curiosity, and a
-          desire to experience everything the world has to offer.    </i>
-          </p>
-
-        <button className="follow-btn">Follow</button>
-      </div>
+    <div>
+      <h2>Fruit List</h2>
+      <ul>
+        {fruits.map((fruit, index) => (
+          <li key={index}>{fruit}</li>
+        ))}
+      </ul>
     </div>
   );
 }
 
+export default FruitList;
