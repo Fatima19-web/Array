695b2bf200633fd9f3c980e12864acbd3c6851b3
fatima <faizamanzoor947@gmail.com>
2025-12-05 21:55:27 +0500

    Your commit message

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-
+// App.js
 import React from "react";
-import "./App.css";
 
-function FruitsList() {
-  const fruits = ["Mango", "Banana", "Apple", "Orange", "Grapes"];
-
+export default 
+function App() {
   return (
-    <div className="fruit-container">
-      <h2 className="fruit-title">Fruit List</h2>
+    <div className="app-wrapper">
+      <div className="card">
+       <img src="/myPic.jpg" alt="My Pic"/>
+      <h2 className="name">Fatima Khan</h2>
 
-      <ul className="fruit-list">
-        {fruits.map((fruit, index) => (
-          <li key={index}>{fruit}</li>
-        ))}
-      </ul>
+        <p><i>A girl who loves to enjoy her life, chasing her dreams with a fearless heart.
+          She believes in exploring new places, meeting new people, and discovering the
+          beauty hidden in every corner of the world. Travelling gives her freedom,
+          learning, and happiness. She lives her life with passion, curiosity, and a
+          desire to experience everything the world has to offer.    </i>
+          </p>
+
+        <button className="follow-btn">Follow</button>
+      </div>
     </div>
   );
 }
-
-export default FruitsList;
-
-
 
